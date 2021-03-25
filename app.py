db55ed4caee709189cdd245212e1fa5a2030e74d
tylerhutcherson <tyler.hutcherson@skafos.ai>
2021-03-24 22:48:32 -0400

    fix app setup

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import asyncio
+import os
 
 from hydrapy import HydraPy
 from lib.processor import Processor
 
 
+CONFIG_PATH = './config/config.json'
+
 class SlackAlertsService:
     hydra = None                # hydra instance
     si = None                   # service info
     processor = Processor()     # message processor
+    redis_connection_string = os.getenv('REDIS_CONNECTION_STRING')
 
     async def main(self):
         service_version = open('VERSION').read().rstrip()
         self.hydra = HydraPy(
-            config_path='./config/config.json',
+            config_path=CONFIG_PATH,
             version=service_version,
             message_handler=self.processor.handle_message,
             queue_handler=None
         )
-        self.si = await self.hydra.init()
+        self.si = await self.hydra.init(self.redis_connection_string)
         await self.processor.init(self.hydra, self.si['serviceName'])
         start_message = f"{self.si['serviceName']}({self.si['instanceID']})(v{self.si['serviceVersion']}) running at {self.si['serviceIP']}"
         print(start_message, flush=True)
